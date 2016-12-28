2fef71bcca88bc20cff5e2aeb998b5e6ede8beaa
Fabien <gamelinfabien@gmail.com>
2016-12-28 18:28:52 +0100

    Developpement 1

--- FourmiliereConsole/Program.cs
+++ FourmiliereConsole/Program.cs
@@ -9,21 +9,32 @@ namespace FourmiliereConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("----- Fourmilière -----");
+            /*      Console.WriteLine("----- Fourmilière -----");
 
-            FabriqueAbstraite uneFabrique = new FabriqueFourmiliere();
+                  FabriqueAbstraite uneFabrique = new FabriqueFourmiliere();
 
-            EnvironnementAbstrait fourmiPalace = uneFabrique.CreerEnvironment();
-            fourmiPalace.ChargerEnvironnement(uneFabrique);
-            fourmiPalace.ChargerPersonnages(uneFabrique);
+                  EnvironnementAbstrait fourmiPalace = uneFabrique.CreerEnvironment();
+                  fourmiPalace.ChargerEnvironnement(uneFabrique);
+                  fourmiPalace.ChargerPersonnages(uneFabrique);
 
-            Console.WriteLine(fourmiPalace.Statistiques());
-          
+                  Console.WriteLine(fourmiPalace.Statistiques());
+
+                  for (int i = 0; i < 10; i++)
+                  {
+                      Console.WriteLine(fourmiPalace.Simuler());
+                  }*/
+
+            Console.WriteLine("----- Metro -----");
+            FabriqueAbstraite depotMetro = new FabriqueMetro();
+            EnvironnementAbstrait Metro = depotMetro.CreerEnvironment();
+            Metro.ChargerEnvironnement(depotMetro);
+            Metro.ChargerPersonnages(depotMetro);
+
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine(fourmiPalace.Simuler());
+                Console.WriteLine(Metro.Simuler());
             }
-            
+
             Console.ReadLine();
         }
     }
