163c5fd736c717ee5d4541f538ad1e780b056706
Ismaïl <nguyen.ismail@gmail.com>
2017-02-07 19:33:01 +0100

    Add launcher with interface

---
+++ MetaSimulateur/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MetaSimulateur
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConsoleFourmiliere_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("FourmiliereConsole.exe");
+        }
+
+        private void buttonConsoleAutoroutes_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("AutoroutesConsole.exe");
+        }
+
+        private void buttonConsoleMetro_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("MetroConsole.exe");
+        }
+    }
+}
--- MetroConsole/Program.cs
+++ MetroConsole/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 using LibAbstraite.Simulateurs;
 using LibMetier.Simulateurs;
 
