220e094837ffffd96349936c06a329b0ee31910d
Ismaïl <nguyen.ismail@gmail.com>
2017-02-07 18:39:57 +0100

    Add json output for metro simulation

--- LibMetier/GestionEnvironnement/EnvironnementMetro.cs
+++ LibMetier/GestionEnvironnement/EnvironnementMetro.cs
@@ -2,6 +2,10 @@
 using System.Text;
 using LibAbstraite.GestionPersonnages;
 using LibAbstraite.Fabriques;
+using Newtonsoft.Json;
+using System;
+using LibMetier.GestionPersonnages.Metro;
+using LibMetier.GestionEnvironnement.Metro;
 
 namespace LibMetier.GestionEnvironnement
 {
@@ -34,6 +38,71 @@ namespace LibMetier.GestionEnvironnement
             return text.ToString();
         }
 
+        public string SimulerJson()
+        {
+            var text = new StringBuilder();
+            Random rnd = new Random();
+            int flag = 0;
+            string jsonoutput;
+
+             foreach(Train unTrain in PersonnagesList)
+              {  
+                foreach (Rails rail in AccesAbstraitsList)
+                {
+                    if (AccesAbstraitsList.Count > 0)
+                    {
+                        if (rail.Debut.Nom == "Nation")
+                        {
+                            unTrain.Passage = DateTime.Now;
+                            for (int i = 0; i < AccesAbstraitsList.Count; i++)
+                            {
+                                if(flag==1) unTrain.Passage = unTrain.Passage.AddMinutes(4);
+                                else unTrain.Passage = unTrain.Passage.AddMinutes(2);
+                                unTrain.nbPassager = rnd.Next(1, 100);
+                                DeplacerPersonnage(unTrain, AccesAbstraitsList[i].Debut, AccesAbstraitsList[i].Fin);
+                              //  sb.AppendFormat("{0} : {1} --> {2}, nb passager : {3}, heure de passage : {4}  \n", unTrain.Nom, RailsList[i].Debut.Nom,
+                           //     RailsList[i].Fin.Nom, unTrain.nbPassager, unTrain.passage);
+                                if(i != AccesAbstraitsList.Count)
+                                 i++;
+                                flag = 1;
+                                jsonoutput = JsonConvert.SerializeObject(unTrain,new JsonSerializerSettings() { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
+                                text.Append(jsonoutput);
+                            }
+                            text.Append("terminus, en attente de départ \n\n\n");
+                        }
+                        if (rail.Debut.Nom == "Mairie de Montreuil")
+                        {
+                            unTrain.Passage = DateTime.Now;
+                            for (int i = AccesAbstraitsList.Count -1; i >= 0; i--)
+                            {
+                                unTrain.nbPassager = rnd.Next(1, 100);
+                                if (flag == 1) unTrain.Passage = unTrain.Passage.AddMinutes(4);
+                                else unTrain.Passage = unTrain.Passage.AddMinutes(2);
+                                DeplacerPersonnage(unTrain, AccesAbstraitsList[i].Debut, AccesAbstraitsList[i].Fin);
+                              //  sb.AppendFormat("{0} : {1} --> {2}, nb passager : {3}, heure de passage : {4} \n", unTrain.Nom, RailsList[i].Debut.Nom,
+                             //   RailsList[i].Fin.Nom, unTrain.nbPassager, unTrain.passage);
+                                if (i != 0)
+                                    i--;
+                                jsonoutput = JsonConvert.SerializeObject(unTrain, Formatting.None, new JsonSerializerSettings() { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
+                                text.Append(jsonoutput);
+                            }
+                            text.Append("terminus, en attente de départ \n\n\n");
+                        }
+
+
+                    }
+                    else
+                    {
+                        text.AppendFormat("{0} : au depot \n", unTrain.Nom);
+                    }
+                }
+              }
+            
+            //RETOURNER JSON
+            
+            return text.ToString();
+        }
+        
         public void AjouteTrain(PersonnageAbstrait unTrain, string depart)
         {
             var pos = int.Parse(depart);
--- LibMetier/GestionPersonnages/Metro/Train.cs
+++ LibMetier/GestionPersonnages/Metro/Train.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using LibAbstraite.GestionEnvironnement;
 using LibMetier.GestionEnvironnement.Metro;
+using System;
 
 namespace LibMetier.GestionPersonnages.Metro
 {
     public class Train : PersonnageAbstrait
     {
+        public DateTime Passage { get; set; }
+
         public override ZoneAbstraite ChoixZoneSuivante(List<AccesAbstrait> accesList)
         {
             return accesList[((Station)Position).Id].Fin;
